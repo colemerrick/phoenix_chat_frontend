7c19bba47daaaab3fde3d727f9493f000cb2aa6d
Cole Merrick <colegmerrick@gmail.com>
2016-09-09 18:22:48 -0700

    presence list

--- app/components/Chat/index.js
+++ app/components/Chat/index.js
@@ -2,20 +2,64 @@ import React from "react"
 import cssModules from "react-css-modules"
 import style from "./style.css"
 
+import { Socket, Presence } from "phoenix"
+import { connect } from "react-redux"
+
 import Sidebar from "../Sidebar"
 
+
 export class Chat extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      presences: {}
+    }
+  }
+
+  componentDidMount() {
+    const params = this.props.user
+    this.socket = new Socket("ws://localhost:4000/socket", { params })
+    this.socket.connect()
+    this.configureAdminChannel()
+  }
+
+  configureAdminChannel() {
+    this.adminChannel = this.socket.channel("admin:active_users")
+
+    this.adminChannel.on("presence_state", state => {
+      const presences = Presence.syncState(this.state.presences, state)
+      this.setState({ presences })
+    })
+
+    this.adminChannel.on("presence_diff", state => {
+      const presences = Presence.syncDiff(this.state.presences, state)
+      this.setState({ presences })
+    })
+
+    this.adminChannel.join()
+      .receive("ok", () => {
+        console.log("Succesfully joined the active_users topic.")
+      })
+  }
+
   render() {
     return (
       <div>
-        <Sidebar />
+        <Sidebar
+          presences={this.state.presences} />
         <div className={style.chatWrapper}>
           chat me
         </div>
-        {this.props.children}
+        { this.props.children }
       </div>
     )
   }
 }
 
-export default cssModules(Chat, style)
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  }
+}
+
+export default connect(mapStateToProps)(cssModules(Chat, style))
